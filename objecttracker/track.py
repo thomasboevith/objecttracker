5d0eb110ebed01b5da92050faa0fc2ac2291b42e
Hallgeir Wilhelmsen <wilhelmsen@gmail.com>
2014-11-20 15:05:59 +0100

    Adding some parents functionality.

--- objecttracker/track.py
+++ objecttracker/track.py
@@ -1,39 +1,85 @@
 import numpy as np
-import itertools
-
-
-def pairwise(iterable):
-    "Pairwise iteration: s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = itertools.tee(iterable)
-    next(b, None)
-    return itertools.izip(a, b)
+import trackpoint
 
 
 class Track:
     def __init__(self):
         self.parent = None
         self.trackpoints = []
-        self.id = None
-        self.parent_id = None
-        self.children_ids = []
+        self.name = None
 
     def add_trackpoint(self, trackpoint):
+        """
+        Adding a trackpoint to the track.
+        """
+        assert(isinstance(trackpoint, trackpoint.TrackPoint))
         self.trackpoints.append(trackpoint)
 
     def set_parent(self, parent):
+        """
+        If a track splits up into two new tracks, the parent track
+        should be connected to the new tracks.
+        """
+        assert(isinstance(parent, Track))
         self.parent = parent
 
+    def split(self):
+        """
+        Splits the track into two new tracks.
+        """
+        return self._cut(), self._cut()
+
+    def _cut(self):
+        """
+        Creates a new track..
+        """
+        t = Track()
+        t.set_parent(self)
+        return t
+
     def length(self):
-        sum = 0
-        if self.number_of_trackpoints() > 0:
-            for tp0, tp1 in pairwise(self.trackpoints):
-                sum += np.sqrt((tp1.row - tp0.row)**2 + (tp1.col - tp0.col)**2)
-        return sum
+        """
+        The length of a track, including its parent track.
+
+        Recursively calls it self on the parents, if any.
+
+        Example:
+        If whe have a track with 4 trackpoints:
+        track = [tp1, tp2, tp3, tp4]
+        It is the distance between tp1 and tp2, plus the distance between
+        t2 and t3, plus the distance between t3 and t4.
+        """
+        # The length of all the parents.
+        parent_length = 0
+        if self.parent != None and len(self.parent.trackpoints) > 0:
+            parent_length += self.parent.length()
+
+            # The length between the last parent tracpoint and the this
+            # first trackpoint.
+            last_parent_tp = self.parent.trackpoints[-1]
+            parent_length += last_parent_tp.length_to(self.trackpoints[0])
+
+        return parent_length + sum(map(
+                lambda (tp0, tp1): tp0.length_to(tp1),
+                zip(self.trackpoints, self.trackpoints[1:])
+                ))
 
     def number_of_trackpoints(self):
-        return len(self.trackpoints)
+        """
+        Returns the number of trackpoints for the track.
+        """
+        number_of_trackpoints = 0
+        if self.parent != None:
+            number_of_trackpoints = self.parent.number_of_trackpoints()
+        return number_of_trackpoints + len(self.trackpoints)
 
     def save(self, filename):
-        with open(filename, 'w') as fp:
+        """
+        Saves the trackpoints to a file, including the parent track.
+        """
+        if self.parent != None:
+            self.save(filename)
+
+        with open(filename, 'a') as fp:
             for trackpoint in self.trackpoints:
-                fp.write(trackpoint)
+                fp.write(str(trackpoint))
--- objecttracker/trackpoint.py
+++ objecttracker/trackpoint.py
@@ -5,7 +5,17 @@ class TrackPoint:
         self.size = size
         self.color = color
         self.shape = shape
-        
+
+    def length_to(self, tp):
+        """
+        Calculate the lengt between two track points.
+        """
+        assert(isinstance(tp), TrackPoint)
+        return np.sqrt((self.row - tp.row) ** 2 + (self.col - tp.col) ** 2)
 
     def __str__(self):
-        return "({row}, {col}) {size} {color} {shape}".format(row=self.row, col=self.col, size=self.size, color=self.color, shape=self.shape)
+        return "({row}, {col}) {size} {color} {shape}".format(row=self.row,
+                                                              col=self.col,
+                                                              size=self.size,
+                                                              color=self.color,
+                                                              shape=self.shape)
