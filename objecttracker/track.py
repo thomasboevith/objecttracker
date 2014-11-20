0189080937cc8b6c00ed7e5bd94f26e4e7a53be4
Thomas Bøvith <1975thomas@gmail.com>
2014-11-20 01:35:34 +0100

    Implemented length method using new function pairwise using itertools

--- objecttracker/track.py
+++ objecttracker/track.py
@@ -1,3 +1,14 @@
+import numpy as np
+import itertools
+
+
+def pairwise(iterable):
+    "Pairwise iteration: s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return itertools.izip(a, b)
+
+
 class Track:
     def __init__(self):
         self.parent = None
@@ -8,22 +19,21 @@ class Track:
 
     def add_trackpoint(self, trackpoint):
         self.trackpoints.append(trackpoint)
-    
+
     def set_parent(self, parent):
         self.parent = parent
 
     def length(self):
         sum = 0
-        raise NotImplementedError("Implement the geographic length of the track in pixels. Do it! Do it!")
+        if self.number_of_trackpoints() > 0:
+            for tp0, tp1 in pairwise(self.trackpoints):
+                sum += np.sqrt((tp1.row - tp0.row)**2 + (tp1.col - tp0.col)**2)
+        return sum
 
     def number_of_trackpoints(self):
         return len(self.trackpoints)
-        
 
     def save(self, filename):
         with open(filename, 'w') as fp:
             for trackpoint in self.trackpoints:
                 fp.write(trackpoint)
-        
-        
-        
