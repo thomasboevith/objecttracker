14a8e486db701e4242011cad273d9f11de31813a
Hallgeir Wilhelmsen <wilhelmsen@gmail.com>
2014-11-21 16:18:22 +0100

    Adding direction between two trackpoints.

--- objecttracker/trackpoint.py
+++ objecttracker/trackpoint.py
@@ -16,6 +16,18 @@ class Trackpoint:
         assert(isinstance(tp, Trackpoint))
         return np.sqrt((self.row - tp.row) ** 2 + (self.col - tp.col) ** 2)
 
+    def direction_to(self, tp, deg=False):
+        """
+        Calculate the direction to the input point.
+        If deg is set to True, the output is in degrees.
+        """
+        assert(isinstance(tp, Trackpoint))
+        # archtan(x/y)
+        direction = np.arctan2((tp.row - self.row), (tp.col-self.col))
+        if deg:
+            direction = np.rad2deg(direction)
+        return direction
+
     def __str__(self):
         return "({row}, {col}) {size} {color} {shape}".format(row=self.row,
                                                               col=self.col,
